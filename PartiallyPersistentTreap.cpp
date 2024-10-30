bfc277e4d14f1ef475ad85686a8568ee5c2b1467
Umesh Konduru <51374009+UmeshKonduru@users.noreply.github.com>
2024-10-30 23:21:59 +0530

    not wasting creative energy on commit messages

--- PartiallyPersistentTreap.cpp
+++ PartiallyPersistentTreap.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <random>
 #include <memory>
+#include <variant>
 #include <map>
 
 using namespace std;
@@ -15,15 +16,13 @@ struct Modification;
 struct Node;
 struct Treap;
 
-struct Modifcation {
+struct Modification {
 
     ModType type;
     int version;
-    
-    union {
-        int value;
-        shared_ptr<Node> node;
-    } data;
+    variant<int, shared_ptr<Node>> data;
+
+    Modification() : type(EMPTY) {}
 };
 
 struct Node {
@@ -32,11 +31,41 @@ struct Node {
     int value;
     shared_ptr<Node> left, right;
     shared_ptr<Modification> mod;
+
+    Node(int value) :
+        weight(rnd()),
+        value(value),
+        mod(make_shared<Modification>()) {}
+
+    Node(const Node& node) : weight(node.weight) {
+
+        value = node.value;
+        left = node.left;
+        right = node.right;
+
+        switch(node.mod->type) {
+            case VALUE:
+            value = get<int>(node.mod->data);
+            break;
+            case LEFT:
+            left = get<shared_ptr<Node>>(node.mod->data);
+            break;
+            case RIGHT:
+            right = get<shared_ptr<Node>>(node.mod->data);
+        }
+
+        mod = make_shared<Modification>();
+    }
+
+    shared_ptr<Node> clone() {
+        return make_shared<Node>(*this);
+    }
 };
 
 struct Treap {
-
     
+    int latestVersion;
+    map<int, shared_ptr<Node>> roots;
 };
 
 int main() {
