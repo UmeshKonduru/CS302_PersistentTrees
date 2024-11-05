122f9dbb14604cc21a8b71a3a9e17fc3bc25f1a0
Umesh Konduru <51374009+UmeshKonduru@users.noreply.github.com>
2024-11-06 01:21:06 +0530

    added copy method and getters for child poitners

--- PartialPersistence.cpp
+++ PartialPersistence.cpp
@@ -8,23 +8,66 @@ enum Color {
 };
 
 enum ModType {
-    LEFT, RIGHT
+    EMPTY, LEFT, RIGHT
 };
 
 struct Node;
 struct Modification;
 
 struct Modification {
+
     ModType type;
     int version;
     shared_ptr<Node> node;
+
+    Modification() : 
+        type(EMPTY),
+        version(-1),
+        node(nullptr) {}
 };
 
 struct Node {
+
     int key;
     Color color;
     shared_ptr<Node> left, right;
     shared_ptr<Modification> mod;
+
+    Node(int key) : 
+        key(key), 
+        color(RED),
+        left(nullptr),
+        right(nullptr),
+        mod(make_shared<Modification>()) {}
+
+    shared_ptr<Node> copy() {
+
+        auto node = make_shared<Node>(key);
+        node->color = color;
+        node->left = left;
+        node->right = right;
+        node->mod = nullptr;
+
+        switch(mod->type) {
+            case LEFT:
+            node->left = mod->node;
+            break;
+            case RIGHT:
+            node->right = mod->node;
+        }
+
+        return node;
+    }
+
+    shared_ptr<Node> getLeft(int version) {
+        if(mod->type == LEFT && mod->version <= version) return mod->node;
+        return left;
+    }
+
+    shared_ptr<Node> getRight(int version) {
+        if(mod->type == RIGHT && mod->version <= version) return mod->node;
+        return right;
+    }
 };
 
 int main() {
